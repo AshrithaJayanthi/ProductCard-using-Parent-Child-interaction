08d8633b8c9ebe8514a3f6db4d91e00df31edb7b
Ashritha Jayanthi <ashrithajayanthi11@gmail.com>
2021-05-07 20:01:21 +0530

    parent child interaction

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -29,18 +29,18 @@ export class ProductsComponent  {
     },
     
     {
-    productTitle:"Technology-4",
-    description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
+    productTitle:"Eye Cream",
+    description:"20$",
     productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/eye-cream_900x.jpg?v=1569447632"
     },
     {
-      productTitle:"Technology-5",
-      description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
+      productTitle:"Vitamin C serum",
+      description:"47$",
       productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/serum_900x.jpg?v=1569447632"
       },
       {
         productTitle:"Technology-6",
-        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore amet dolor iusto",
+        description:"23$",
         productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/KS_Lip-Oil_Ecomm-Pink_Websized_01_c1ec492a-2914-4ff4-b78c-d8a22cea31c6_900x.jpg?v=1612905125"
         },
 
