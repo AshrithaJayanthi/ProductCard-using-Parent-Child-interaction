4b3fa8e1e527d27505ce2a358b9ee60ce082b2ef
Ashritha Jayanthi <ashrithajayanthi11@gmail.com>
2021-05-08 18:24:00 +0530

    changes are made in the product details

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,43 +10,45 @@ export class ProductsComponent  {
   //when data is not dynamic and same data is there everywhere
   // products=[1,2,3,4]
   products:Product[]=[
-
+    //declaring 6 new productDetails
     {
-    productTitle:"Serum Bundle",
-  description:"56$",
-    productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/SERUM-BUNDLE-WS_900x.jpg?v=1600442327"
+    productTitle:"Coco Mademoiselle Parfum Spray",
+  description:"84$",
+    productImage:"https://www.chanel.com/images/w_0.51,h_0.51,c_crop/f_auto,w_512,h_512/coco-mademoiselle-eau-de-parfum-spray-1-2fl-oz--packshot-default-116390-8824196956190.jpg"
     },
     {
-    productTitle:"Foaming facewash",
-    description:"20$",
-    productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/FOAM-FACE-WASH-ECOMM-ON-PINK_900x.jpg?v=1599085287"
-    },
-    
-    {
-    productTitle:"Face Moisturizer",
-    description:"26$",
-    productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/face-moisturizer_900x.jpg?v=1569447632"
+    productTitle:"N°5 Eau de Parfum Spray",
+    description:"83$",
+    productImage:"https://www.chanel.com/images//t_one//w_0.51,h_0.51,c_crop/q_auto:good,f_jpg,fl_lossy,dpr_1.2/w_620/n-5-eau-de-parfum-spray-1-2fl-oz--packshot-default-125230-8833876000798.jpg"
     },
     
     {
-    productTitle:"Eye Cream",
-    description:"20$",
-    productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/eye-cream_900x.jpg?v=1569447632"
+    productTitle:"Coco Mademoiselle Intense Spray",
+    description:"85$",
+    productImage:"https://www.chanel.com/images//t_one//w_0.51,h_0.51,c_crop/q_auto:good,f_jpg,fl_lossy,dpr_1.2/w_620/coco-mademoiselle-eau-de-parfum-intense-spray-1-2fl-oz--packshot-default-116630-8817976377374.jpg"
+    },
+    
+    {
+    productTitle:"Chance Parfum Spray",
+    description:"85$",
+    productImage:"https://www.chanel.com/images//t_one//w_0.51,h_0.51,c_crop/q_auto:good,f_jpg,fl_lossy,dpr_1.2/w_620/chance-eau-tendre-eau-de-parfum-spray-1-2fl-oz--packshot-default-126240-8820439121950.jpg"
     },
     {
-      productTitle:"Vitamin C serum",
-      description:"47$",
-      productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/serum_900x.jpg?v=1569447632"
+      productTitle:"Chance Toilette Spray",
+      description:"82$",
+      productImage:"https://www.chanel.com/images//t_one//w_0.51,h_0.51,c_crop/q_auto:good,f_jpg,fl_lossy,dpr_1.2/w_620/chance-eau-fraiche-eau-de-toilette-spray-1-7fl-oz--packshot-default-136410-8833882095646.jpg"
       },
       {
-        productTitle:"Technology-6",
-        description:"23$",
-        productImage:"https://cdn.shopify.com/s/files/1/0066/8719/5183/products/KS_Lip-Oil_Ecomm-Pink_Websized_01_c1ec492a-2914-4ff4-b78c-d8a22cea31c6_900x.jpg?v=1612905125"
+        productTitle:"Chance Eau de Parfum Spray",
+        description:"108$",
+        productImage:"https://www.chanel.com/images//t_one//w_0.55,h_0.55,c_crop/q_auto:good,f_jpg,fl_lossy,dpr_1.2/w_620/chance-eau-de-toilette-spray-3-4fl-oz--packshot-default-126460-8833883176990.jpg"
         },
 
   ];
   productsSentByChild=[]
+  //initialising count to 0
   productCount:number=0;
+  //to push items into the cart array
   getProductDetailsFromChild(productTitle)
   {
 this.productsSentByChild.push(productTitle)
